0ecca665aa0356300c66822936272910501ff2fc
mmjamati <mukhlis.jamato@gmail.com>
2015-10-12 18:33:44 -0600

    add stuffs

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@
 
 // App Module: the name AngularStore matches the ng-app attribute in the main <html> tag
 // the route provides parses the URL and injects the appropriate partial page
-var storeApp = angular.module('AngularStore', []).
-  config(['$routeProvider', function($routeProvider) {
+var storeApp = angular.module('app', ['ngRoute']);
+
+storeApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/store', {
         templateUrl: 'partials/store.htm',
-        controller: storeController 
+        controller: storeController
+
       }).
       when('/products/:productSku', {
         templateUrl: 'partials/product.htm',
